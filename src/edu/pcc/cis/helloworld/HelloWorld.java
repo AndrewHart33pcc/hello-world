19d59f7ff6a62cb4910fda9ef73e9c8b7d39082e
Andrew Hart <andrew.hart3@pcc.edu>
2022-10-23 10:33:36 -0700

    Added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Author: Andrew Hart
+ * Version: 10/23/2022
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
